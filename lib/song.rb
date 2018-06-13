6b344d8661f0ed49c6fb92039beea8f128525d50
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-13 17:47:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,8 @@ class Song
 
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
-  include Paramable::InstanceMethods 
-  include Memorable::InstanceMethods 
+  include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
 
   @@songs = []
 
