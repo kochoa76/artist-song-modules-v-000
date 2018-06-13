784545bed2c44ad1454cacb3b11391b8a0abeafe
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-13 17:40:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,36 +1,29 @@
 require 'pry'
 
+
 class Song
-  attr_accessor :name
-  attr_reader :artist
 
-  @@songs = []
+      attr_accessor :name
+      attr_reader :artist
 
-  def initialize
-    @@songs << self
-  end
+      extend Memorable::ClassMethods
+      extend Findable::ClassMethods
+      
+      @@songs = []
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+      def initialize
+        @@songs << self
+      end
 
-  def artist=(artist)
-    @artist = artist
-  end
+      def artist=(artist)
+        @artist = artist
+      end
 
-  def self.reset_all
-    @@songs.clear
-  end
+      def to_param
+        name.downcase.gsub(' ', '-')
+      end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-
-  def self.count
-    self.all.count
-  end
-
-  def self.all
-    @@songs
-  end
-end
+      def self.all
+        @@songs
+      end
+    end
